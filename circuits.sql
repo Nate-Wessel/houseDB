072818ecb26671e05978329efc32fa7fdafa39f6
Nate Wessel <nate@natewessel.com>
2024-11-03 16:34:05 -0500

    define electrical circuits

---
+++ circuits.sql
@@ -0,0 +1,8 @@
+CREATE TABLE circuits (
+	uid text PRIMARY KEY,
+	panel text NOT NULL REFERENCES panels (short_name),
+	amps smallint NOT NULL CHECK (amps > 0),
+	gfci boolean,
+	afci boolean,
+	linked_with text REFERENCES circuits (uid) -- linkages or double breakers
+);
