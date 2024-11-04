76925ab0858a7792ab155be16ab3ee8aa7f618f0
Nate Wessel <nate@natewessel.com>
2024-11-03 21:28:12 -0500

    rename "room" -> "space"
    Same concept but generalized a bit

--- interfaces.sql
+++ interfaces.sql
@@ -1,8 +1,8 @@
 -- "interfaces" are any point where the wiring comes through
 -- the wall and interacts either as a wire, a switch, or an outlet
 CREATE TABLE interfaces (
-	room text NOT NULL REFERENCES rooms (short_name),
-	location_in_room text NOT NULL,
+	space text NOT NULL REFERENCES spaces (short_name),
+	location_in_space text NOT NULL,
 	circuit text REFERENCES circuits (uid),
 	interface_type text NOT NULL REFERENCES interface_types (short_name),
 	grounded boolean,
--- spaces.sql
+++ spaces.sql
@@ -1,4 +1,4 @@
-CREATE TABLE rooms (
+CREATE TABLE spaces (
 	short_name text PRIMARY KEY,
 	floor text NOT NULL REFERENCES floors (uid),
 	description text
