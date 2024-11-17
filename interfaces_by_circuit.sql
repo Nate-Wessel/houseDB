dac1e4a54f68da84571e10e8473f0d4c8b8bfd1b
Nate Wessel <nate@natewessel.com>
2024-11-17 18:52:58 -0500

    count unknown interfaces

--- interfaces_by_circuit.sql
+++ interfaces_by_circuit.sql
@@ -4,8 +4,9 @@ SELECT
 	circuits.uid AS circuit,
 	circuits.amps,
 	COUNT(DISTINCT interfaces.space || location_in_space || interface_type) AS interface_count,
-	COUNT(DISTINCT interfaces.space || location_in_space || interface_type) FILTER (WHERE interfaces.grounded IS NOT TRUE) AS k_and_t_interfaces,
-	array_agg(DISTINCT space) FILTER (WHERE space IS NOT NULL) AS spaces
+	COUNT(DISTINCT interfaces.space || location_in_space || interface_type) FILTER (WHERE NOT interfaces.grounded) AS ungrounded_interfaces,
+	COUNT(DISTINCT interfaces.space || location_in_space || interface_type) FILTER (WHERE interfaces.grounded IS NULL) AS interfaces_grounding_unknown,
+	array_agg(DISTINCT space ORDER BY space) FILTER (WHERE space IS NOT NULL) AS spaces
 FROM circuits
 LEFT JOIN interfaces ON interfaces.circuit = circuits.uid
 GROUP BY
