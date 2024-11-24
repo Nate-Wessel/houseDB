735a413e4a823663421340b624e0c2f6a524ad4c
Nate Wessel <nate@natewessel.com>
2024-11-24 09:29:31 -0500

    add voltage column to circuits

--- circuits.sql
+++ circuits.sql
@@ -4,5 +4,6 @@ CREATE TABLE circuits (
 	amps smallint NOT NULL CHECK (amps > 0),
 	gfci boolean,
 	afci boolean,
-	linked_with text REFERENCES circuits (uid) -- linkages or double breakers
+	linked_with text REFERENCES circuits (uid), -- linkages or double breakers
+	voltage smallint
 );
