36be742925b17b129840bc591ad4cec4290df663
Nate Wessel <nate@natewessel.com>
2025-08-17 11:59:37 -0400

    indicate whether circuit has had rewiring done

--- interfaces_by_circuit.sql
+++ interfaces_by_circuit.sql
@@ -3,6 +3,10 @@ CREATE OR REPLACE VIEW interfaces_by_circuit AS
 SELECT
 	circuits.uid AS circuit,
 	circuits.amps,
+	COALESCE(
+		bool_or(rewired),
+		FALSE
+	) AS has_rewired_interfaces,
 	COUNT(DISTINCT interfaces.space || location_in_space || interface_type) AS interface_count,
 	COUNT(DISTINCT interfaces.space || location_in_space || interface_type) FILTER (WHERE NOT interfaces.grounded) AS ungrounded_interfaces,
 	COUNT(DISTINCT interfaces.space || location_in_space || interface_type) FILTER (WHERE interfaces.grounded IS NULL) AS interfaces_grounding_unknown,
