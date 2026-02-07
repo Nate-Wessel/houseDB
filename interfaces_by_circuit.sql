b63b48d7ea71d002f94fbd50c8ba30cd76c82c0c
Nate Wessel <nate@natewessel.com>
2026-02-07 18:50:27 -0500

    also list distinct floors per circuit

--- interfaces_by_circuit.sql
+++ interfaces_by_circuit.sql
@@ -10,9 +10,11 @@ SELECT
 	COUNT(DISTINCT interfaces.space || location_in_space || interface_type) AS interface_count,
 	COUNT(DISTINCT interfaces.space || location_in_space || interface_type) FILTER (WHERE NOT interfaces.grounded) AS ungrounded_interfaces,
 	COUNT(DISTINCT interfaces.space || location_in_space || interface_type) FILTER (WHERE interfaces.grounded IS NULL) AS interfaces_grounding_unknown,
-	array_agg(DISTINCT space ORDER BY space) FILTER (WHERE space IS NOT NULL) AS spaces
+	array_agg(DISTINCT space ORDER BY space) FILTER (WHERE space IS NOT NULL) AS spaces,
+	array_agg(DISTINCT spaces.floor ORDER BY spaces.floor) AS floors
 FROM circuits
 LEFT JOIN interfaces ON interfaces.circuit = circuits.uid
+LEFT JOIN spaces ON interfaces.space = spaces.short_name
 GROUP BY
 	circuits.uid,
 	circuits.amps
