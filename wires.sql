3f47181b97dca0253c28c720ece7243e229c001c
Nate Wessel <nate@natewessel.com>
2026-03-02 21:53:50 -0500

    indicate whether 2 or 3 conductor wire

--- wires.sql
+++ wires.sql
@@ -2,6 +2,7 @@ CREATE TABLE wires (
 	connects_from smallint NOT NULL REFERENCES interfaces (uid),
 	connects_to smallint NOT NULL REFERENCES interfaces (uid),
 	gauge smallint,
+	conductors smallint NOT NULL DEFAULT 2,
 	ground boolean DEFAULT TRUE,
 	notes text,
 	CHECK (connects_from != connects_to),
